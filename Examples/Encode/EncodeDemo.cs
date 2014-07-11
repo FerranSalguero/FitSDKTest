5f9aa0950fe2436a6e13b9abe9849324bd8d180b
Ferran Salguero Mateu <fsalgueromateu@gmail.com>
2014-07-11 17:04:22 +0200

    Encode fit from gpx

--- Examples/Encode/EncodeDemo.cs
+++ Examples/Encode/EncodeDemo.cs
@@ -21,6 +21,7 @@ using System.Diagnostics;
 using Dynastream.Fit;
 using fit = Dynastream.Fit;
 using SharpGpx;
+using System.Linq;
 
 namespace EncodeDemo
 {
@@ -47,38 +48,10 @@ namespace EncodeDemo
             var route = GpxClass.FromFile("route.gpx");
 
             CourseMesg course = new CourseMesg();
-            course.SetName(Encoding.UTF8.GetBytes(route.metadata.name));
+            course.SetName(Encoding.UTF8.GetBytes("route from gpx"));
             course.SetSport(Sport.Cycling);
 
             var baseDate = route.metadata.timeSpecified ? route.metadata.time : System.DateTime.Now.AddDays(-1);
-
-            var lap = new LapMesg();
-            lap.SetTimestamp(new fit.DateTime(baseDate));
-            lap.SetStartPositionLat(491829325);
-            lap.SetStartPositionLong(12838824);
-            lap.SetEndPositionLat(491829325);
-            lap.SetEndPositionLong(12838824);
-
-            var e = new EventMesg();
-            e.SetTimestamp(new fit.DateTime(baseDate));
-            e.SetEventType(EventType.Start);
-            e.SetEventGroup(0);
-            e.SetEvent(Event.Timer);
-            e.SetData(4294967295);
-
-            var eventStop = new EventMesg();
-            eventStop.SetData(4294967295);
-            eventStop.SetTimestamp(new fit.DateTime(baseDate.AddSeconds(30)));
-            eventStop.SetEvent(Event.Timer);
-            eventStop.SetEventType(EventType.StopDisableAll);
-
-            var point = new RecordMesg();
-            point.SetPositionLat(491829325);
-            point.SetPositionLong(12838824);
-            point.SetDistance(10665.65f);
-            point.SetAltitude(479.4f);
-            point.SetTimestamp(new fit.DateTime(baseDate));
-
 
             FileStream fitDest = new FileStream("Test.fit", FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
 
@@ -92,12 +65,48 @@ namespace EncodeDemo
 
             encodeDemo.Write(course);
 
+            var lap = new LapMesg();
             encodeDemo.Write(lap);
 
+
+            var firstTrk = route.trk.First();
+
+            var firstTrkSeg = firstTrk.trkseg.First();
+
+            var firstPoint = firstTrkSeg.trkpt.First();
+            lap.SetTimestamp(new fit.DateTime(firstPoint.time));
+            lap.SetStartPositionLat(firstPoint.lat.RawInt());
+            lap.SetStartPositionLong(firstPoint.lon.RawInt());
+
+            var lastPoint = firstTrkSeg.trkpt.Last();
+            lap.SetEndPositionLat(lastPoint.lat.RawInt());
+            lap.SetEndPositionLong(lastPoint.lon.RawInt());
+
+            var e = new EventMesg();
+            e.SetTimestamp(new fit.DateTime(firstPoint.time));
+            e.SetEventType(EventType.Start);
+            e.SetEventGroup(0);
+            e.SetEvent(Event.Timer);
+            e.SetData(null);
             encodeDemo.Write(e);
 
-            encodeDemo.Write(point);
+            foreach (var point in firstTrkSeg.trkpt)
+            {
+                var p = new RecordMesg();
+                p.SetPositionLat(point.lat.RawInt());
+                p.SetPositionLong(point.lon.RawInt());
+                //p.SetDistance(point. 10665.65f);
+                p.SetAltitude(Convert.ToSingle(point.ele));
+                p.SetTimestamp(new fit.DateTime(baseDate));
 
+                encodeDemo.Write(p);
+            }
+
+            var eventStop = new EventMesg();
+            eventStop.SetData(null);
+            eventStop.SetTimestamp(new fit.DateTime(lastPoint.time));
+            eventStop.SetEvent(Event.Timer);
+            eventStop.SetEventType(EventType.StopDisableAll);
             encodeDemo.Write(eventStop);
 
             // Update header datasize and file CRC
---
+++ Examples/Encode/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EncodeDemo
+{
+    public static class Extensions
+    {
+        public static int RawInt(this decimal value)
+        {
+            var raw = value.ToString().Replace(".", string.Empty);
+
+            return int.Parse(raw);
+        }
+    }
+}
